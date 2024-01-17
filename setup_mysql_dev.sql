1c8c9e67dda391ca5949245b705242288000bc79
AbduGad <abdo79gad@gmail.com>
2024-01-17 21:53:47 +0200

    ADDED SOME COMMENTS

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+--CREATES A DTABASE
+--CREATES A USER WITH PRIVILEGES
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev@localhost';
